88669d0051603c022993e4415054f0d102f26438
admin <meitangyanyan@gaoxiaobang.com>
2016-05-19 17:14:14 +0800

    readme

---
+++ day2/login.py
@@ -0,0 +1,198 @@
+#/usr/bin/env python
+#-*- coding:utf-8 -*-
+#Authot:Zhang Yan
+
+import sys,time,os,re
+from configparser import ConfigParser
+import smtplib
+from email.mime.text import MIMEText
+
+db = "db.txt"            #定义配置文件(存放邮箱信息,admin和普通用户的信息)
+if not os.path.exists(db):      #如果配置文件不存在
+    sys.exit("不能找到用户配置文件 %s" % db)  #退出系统
+cf = ConfigParser()                     #构造一个configparse对象用来读取配置文件
+cf.read(db)                     #读取配置文件
+userlist=eval(cf.get("admin","userlist"))  #从配置文件中取用户列表
+blacklist=eval(cf.get("admin","blacklist")) #从配置文件中取黑名单列表
+
+mailto_list=[cf.get("mail","mail_to")]     #从配置文件取收件人
+mail_host=cf.get("mail","mail_host")  #设置邮件服务器,从配置文件中取
+mail_user=cf.get("mail","mail_user")    #从配置文件中取发件人用户名
+mail_pass=cf.get("mail","mail_pass")   #从配置文件中取发件人客户端授权密码
+mail_postfix=cf.get("mail","mail_postfix")  #从配置文件中取发件箱的后缀
+
+def send_mail(sub,content,to_list=mailto_list):     #发送邮件函数
+    me="本地部署"+"<"+mail_user.split('@')[0]+"@"+mail_postfix+">"   #定义发件人
+    msg = MIMEText(content,_subtype='plain',_charset='utf-8')       #发送信息
+    msg['Subject'] = sub                                            #发送主题
+    msg['From'] = me                                                #邮件抬头
+    msg['To'] = ";".join(to_list)                                   #邮件抬头
+    try:                                                            #try方法
+        server = smtplib.SMTP()                                     #构造邮件对象
+        server.connect(mail_host)                                   #连接邮件服务器
+        server.login(mail_user,mail_pass)                           #登录邮箱
+        server.sendmail(me, to_list, msg.as_string())               #发送邮件
+        server.close()                                              #关闭邮箱
+        return True                                                 #发送成功返回True
+    except Exception as e:
+        print(str(e))
+        return False                                                #失败返回False
+
+def mail_check():
+    while True:
+        mailto=input("请输入您可以接收邮件的邮箱:")      #输入接收邮件的邮箱
+        if mailto == "":                            #输入不能为空
+            print("必须设置接收邮件的邮箱!")
+        elif len(mailto) > 7 and re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", mailto) != None: #正则检查邮箱输入格式是否正确
+            cf.set("mail", "mail_to", mailto)  #正确的话,将邮箱写入配置文件
+            cf.write(open("userauth.txt", "w"))
+            break           #退出循环
+        else:               #不正确重新输入
+            print("您输入的邮箱格式有误!请重新输入!")
+
+def show(show_flag,name):                        #显示函数
+    if show_flag == "1":                         #如果显示选项是"1",打印下面的内容
+        print('''
+            #################################
+                       欢迎光临商城!
+                    登录       管理员登录
+            #################################
+            ''')
+    elif show_flag == "2":                      #如果显示选项是"2",打印下面的内容
+        print('''
+            ################################
+                     欢迎光临商城!
+                                 %s
+             您可以进行以下操作:
+             A 注销
+             B 修改密码
+             C 退出系统
+             D 开始购物
+            ################################
+        ''' % name)
+    else:                                       #如果显示选项是"3",打印下面的内容
+        print('''
+            ################################
+             管理员:%s恭喜您登录成功!
+             您可以进行以下操作:
+             A 注销
+             B 修改密码
+             C 退出系统
+             D 用户解锁
+            ################################
+        ''' % name)
+
+def login():                                #登录函数
+    locked = {}                             #定义字典locked来存放每个用户的允许失败登录次数
+    for na in userlist:                     #每个用户登录失败3次就被锁定
+        locked[na] = 3
+    show("1","aa")                          #打印提示登录页面
+    login_flag=False
+    while True:
+        name=input("请输入您的登录用户名:")     #登录的用户名
+        passwd=input("请输入您的登录密码:")     #登录的密码
+        if name == "admin":                 #如果是管理员登录
+            print("您好,您正在用管理员账号登录,请确保您是管理员!")
+            print("注意:管理员只有一次登录机会且只能通过邮件解锁!")
+            if passwd == cf.get("admin","password"):  #从配置文件里取管理员的密码和输入密码比较
+                show("3",name)              #如果相同则登录成功,打印管理员的登录信息
+                login_flag=True
+                user(name)                  #调用user()函数进行之后的操作(改密码,解锁等等)
+            else:                           #如果登录不成功
+                st=input("管理员已锁定!是否发送解锁邮件? y/n")  #选择是否联系管理员解锁
+                if st.lower() == "y":                   #选是
+                    if send_mail("解锁邮件", "用户%s申请解锁!\n请点击下面的链接进行解锁:http://jiesu.com" % name):    #给管理员发送邮件
+                        print("管理员解锁函数待开发.......")  #因为管理员不能给管理员解锁,所以打算通过类似一般网站的重置密码方式进行解锁,所以先空着
+                else:   #如果选择不发邮件,因为是管理员登录,所以直接强制退出系统
+                    sys.exit("管理员已被锁定,强制退出系统!")
+        elif name in userlist:        #如果是普通用户登录,判断用户是不是在用户列表里,如果在
+            if name in blacklist:     #判断用户是不是在黑名单列表里,如果在,说明用户之前输入密码错误3次已经被锁定啦
+                st=input("%s 用户已被锁定!请问是否给管理员发解锁邮件解锁? y/n" % name)  #是否联系管理员解锁
+                if st.lower() == "y":   #选是
+                    send_mail("解锁邮件","用户%s申请解锁!" % name)  #给管理员发送邮件
+                else:                   #选否
+                    st = input("是否切换用户? y/n")               #是否切换用户
+                    if st.lower() == "y":                       #选是
+                        continue                                #继续循环
+                    else:                                       #选否
+                        print("此用户将被锁定5秒!请稍后再试")       #用户被锁定5秒后自动解锁(实际情况中这个时间拉长)
+                        for i in range(5):                      #锁定5秒
+                            print("wait.." * (i + 1))
+                            time.sleep(1)
+                        blacklist.remove(name)                  #用户从黑名单里移除,解锁
+                        cf.set("admin", "blacklist", str(blacklist))    #将修改后的黑名单写入配置文件
+                        cf.write(open("userauth.txt", "w"))
+                        continue                                #继续循环
+            else:                                               #如果用户不在黑名单里
+                if passwd == cf.get(name,"password"):           #从配置文件获取用户的密码,如果输入密码与文件中的密码匹配
+                    show("2",name)                              #打印普通用户登录成功信息
+                    login_flag=True
+                    if user(name):                                  #调用user函数,执行之后的操作(改密码等等)
+                        break
+                else:                                           #如果密码不匹配
+                    locked[name]-=1                             #用户允许失败登录次数减1
+                    if locked[name] == 0:                       #如果用户允许登录失败次数变为0
+                        print("您已三次输入密码不正确!%s 用户已被锁定!" % name)           #则输出三次登录失败信息
+                        blacklist.append(name)                  #加入黑名单
+                        cf.set("admin","blacklist",str(blacklist))
+                        cf.write(open("userauth.txt","w"))
+                    else:                                       #如果用户允许登录次数不为0
+                        print("您输入的密码不正确!您还有%d次机会!请重新输入!" % locked[name]) #提示密码输入失败,重新输入
+                        continue
+        else:                       #如果输入name既不是admin又不是普通用户,输出用户未注册,重新输入
+            print("未在库中找到您的用户名%s,请确认您已注册并且输入用户名正确!" % name)
+            continue
+    return login_flag,name
+
+def user(name):                 #user函数
+    while True:
+        if name == "admin":     #如果登录的用户是admin,可输入选项为下:
+            st1 = input("请输入您要进行的操作:A 注销 B 修改密码 C 退出系统 D 用户解锁")
+        else:                   #如果登录的是普通用户,可输入选项为下:
+            st1=input("请输入您要进行的操作:A 注销 B 修改密码 C 退出系统 D 开始购物")
+        if st1.upper() == "A":  #如果输入为A,注销账号
+            print("您已成功注销,3秒后自动跳入首页!")
+            for j in range(3):   #等待3秒跳到首页
+                print("#"*(j+1))
+                time.sleep(1)
+            login()             #调用登录函数,重新开始登录
+        elif st1.upper() == "B":    #如果输入为B,改密码
+            pass_old=input("请输入旧密码:") #先输入旧密码
+            if pass_old == cf.get(name,"password"):  #判断旧密码是不是和文件中保存的一致,如果一致
+                for k in range(3):                   #有三次输入新密码的机会
+                    pass_new=input("请输入您的新密码:") #输入新密码
+                    pass_new2=input("请再次输入您的新密码:") #确认新密码
+                    if pass_new == pass_new2:           #如果两次输入新密码一致
+                        cf.set(name,"password",pass_new)    #将新密码更新进配置文件
+                        cf.write(open("userauth.txt", "w"))
+                        print("恭喜您修改密码成功!Y(^_^)Y")
+                        break
+                    else:                           #如果两次新密码输入不一致
+                        if k == 2:                  #如果三次机会都用完
+                            print("您已三次输入错误!我已严重怀疑您的智商→_→ ")
+                        else:                       #否则重新输入新密码
+                            continue
+            else:                               #如果旧密码输入错误
+                print("很抱歉,您的旧密码输入有误,您无法修改密码!")
+        elif st1.upper() == "C":        #如果输入为C
+            sys.exit("欢迎您的下次光临!=^_^=")
+        elif st1.upper() == "D":        #如果输入为D,分两种情况
+            if name == "admin":         #如果是admin用户输入为D,就是要解锁普通用户
+                username=input("请输入您要解锁的用户:")   #输入要解锁的用户
+                if username in blacklist:               #如果要解锁的用户在黑名单里
+                    blacklist.remove(username)          #解锁
+                    cf.set("admin","blacklist",str(blacklist))
+                    cf.write(open("userauth.txt","w"))
+                    print("用户%s解锁成功!" % username)
+                else:                                   #否则提示输入有误
+                    print("您的输入有误!您输入的用户%s不在黑名单里!" % username)
+            else:
+                print("您将开始购物!")
+                return True   #如果普通用户输入D,则开始购物,即退出user函数
+        else:                                   #输入别的都是输入有误
+            print("您输入的有误,请重新输入!")
+
+# if __name__ == "__main__":
+#     mail_check()
+#     login()  #调用login函数开始登录
+
---
+++ day2/shop.py
@@ -0,0 +1,243 @@
+#/usr/bin/env python
+#-*- coding:utf-8 -*-
+#Authot:Zhang Yan
+from configparser import ConfigParser
+import os,sys,datetime
+from login import mail_check,login
+
+db = "db.txt"            #定义配置文件(存放邮箱信息,admin和普通用户的信息,以及商品库存信息)
+if not os.path.exists(db):      #如果配置文件不存在
+    sys.exit("不能找到用户配置文件 %s" % db)  #退出系统
+cf = ConfigParser()                     #构造一个configparse对象用来读取配置文件
+cf.read(db,encoding="utf8")                     #读取配置文件
+
+goods={
+    "服装":{
+        "女装":[("连衣裙",100),("女裤",80),("女牛仔裤",50),("女T恤",90),("女鞋",50)],
+        "男装":[("男裤",80),("男牛仔裤",50),("男T恤",90),("男鞋",50)]
+    },
+    "通信":{
+        "手机":[("华为手机",3000),("Iphone",6000),("小米手机",1000)],
+        "电脑":[("联想电脑",6000),("华硕电脑",4000),("mac",12000)]
+    }
+}
+#将字典类型的参数转化成列表并返回的函数(将goods函数的每一层都转换成一个列表,实现三级菜单)
+def show(name,flag):
+    menu=[]
+    print("商品列表如下:")
+    if flag == 0:
+        print("编号".ljust(10), "商品")
+        for k, j in enumerate(name):
+            menu.append(j)
+            print(str(k).ljust(12), j)
+    if flag == 1:
+        print("编号", "商品".center(20),"价格")
+        for k, j in enumerate(name):
+            menu.append(j)
+            print(k, j[0].center(30),j[1])
+    return menu
+#商品展示(三级菜单函数)
+def show_goods():
+    print("欢迎光临本商城".center(50, "-"))
+    while True:
+        menu1 = show(goods,0)
+        num1=input("\033[37;1m请问您想查看哪类商品呢?请输入对应的编号:\033[0m")
+        if num1.isdigit() and int(num1) < len(menu1):
+            goods1=goods[menu1[int(num1)]]
+            menu2=show(goods1,0)
+            while True:
+                num2=input("\033[33;1m(b or back返回上一级)\033[0m\033[37;1m请问您想查看哪类商品呢?请输入对应的编号:\033[0m")
+                if num2.isdigit() and int(num2) < len(menu1):
+                    goods2=goods1[menu2[int(num2)]]
+                    menu3 = show(goods2,1)
+                    return menu3
+                elif num2 == "b" or num2 == "back":
+                    break
+                else:
+                    print("您的输入有误！请重新输入！")
+                    continue
+        else:
+            print("您的输入有误！请重新输入！")
+            continue
+#购物函数(此购物函数可以选择立即下单和加入购物车两种模式,立即下单会立即结算,购买的商品信息会保存到db.txt文件的对应用户的order变量里,购物车商品信息会保存到cart变量里)
+#(此购物函数允许用户一次购买多个商品,还可以判断商品的库存是否充足,商品的库存保存在db.txt文件的"库存"的对应商品名的变量里)
+def shop_func(user):
+    balance=int(cf.get(user,"salary"))
+    cart=eval(cf.get(user,"cart"))
+    order=eval(cf.get(user,"order"))
+    exit_flag=False
+    goods_list = show_goods()
+    while not exit_flag:
+        kind=input("\033[31;1m您想查看别的分类的商品吗？y/n\033[0m")
+        if kind == "y":
+            goods_list = show_goods()
+            continue
+        elif kind == "n":
+            pass
+        else:
+            print("您输入的有误，请重新输入！")
+            continue
+        num = input("\033[33;1m(q as quit)\033[0m\033[37;1m请输入您想购买的商品的编号:\033[0m")
+        if num == "q" or num == "quit":
+            print("您购买的商品有:")
+            for index, items in enumerate(order):
+                print("[%d] 商品[%s]  数量[%d]" % (index,items[0], items[1]))
+            print("您的购物车未结算商品有：")
+            for index, items in enumerate(cart):
+                print("商品[%s]  数量[%d]" % (items[0], items[1]))
+            print("您的余额为\033[31;1m[%d]\033[0m" % balance)
+            cf.set(user,"salary",str(balance))
+            cf.set(user,"cart",str(cart))
+            cf.set(user,"order",str(order))
+            cf.set("库存",p_name,str(p_stock))
+            cf.write(open(db,"w"))
+            exit_flag = True
+        elif num.isdigit() and int(num) < len(goods_list):
+            num = int(num)
+            p_name = goods_list[num][0]
+            p_price = goods_list[num][1]
+            p_stock = int(cf.get("库存", p_name))
+            print("您要购买的商品的库存是[%d]" % p_stock)
+            buy_num = input("\033[37;1m请输入您要购买的商品的数量:\033[0m")
+            buy_num = int(buy_num)
+            if p_stock >= buy_num:
+                while True:
+                    settle=input("\033[37;1m您想对商品[%s]立即下单还是加入购物车呢? y立即购买/n加入购物车\033[0m" % p_name)
+                    if settle.lower() == "y":
+                        total=p_price * buy_num
+                        if total <= balance:
+                            balance -= total
+                            buy_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                            goods_buy=(p_name,buy_num,buy_time)
+                            order.append(goods_buy)
+                            p_stock-=buy_num
+                            print("恭喜您购买成功！详细信息请点击我的订单查询！")
+                        else:
+                            print("您的余额为[%d] 不足以支付!" % balance)
+                        break
+                    elif settle.lower() == "n":
+                        goods_cart = [p_name, buy_num, p_price]
+                        cart.append(goods_cart)
+                        print("您的商品[%s]已加入购物车,您随时都可以对它进行结算!" % p_name)
+                        break
+                    else:
+                        print("您输入的有误，请重新输入！")
+                        continue
+            else:
+                print("您选购的商品[%s]库存不足，请选择其他商品！" % p_name)
+#查看用户购物记录函数
+def record_func(user):
+    record=eval(cf.get(user,"order"))
+    for index,items in enumerate(record):
+        print("\033[31;1m[%d]\033[0m [%s]您好，您在[%s]买了[%d]件商品[%s]" % (index,user,items[2],items[1],items[0]))
+#购物车函数(此函数可以实现查看购物车,修改购物车,购物车商品结算,清空购物车等功能)
+def cart_func(user):
+    cart=eval(cf.get(user,"cart"))
+    order=eval(cf.get(user,"order"))
+    balance=int(cf.get(user,"salary"))
+    print("\033[33;7m您的购物车如下\033[0m".center(50))
+    print("编号", "商品".center(20), "数量".center(20),"价格")
+    for index,items in enumerate(cart):
+        print(index,items[0].center(20),str(items[1]).center(20),items[2])
+    exit_flag=False
+    while not exit_flag:
+        opera=input("请输入您要进行的操作：A 结算 B 修改购物车 C 清空购物车 D 退出购物车 E 查看购物车")
+        if opera.upper() == "A":
+            total=0
+            stock_dict={}
+            for goods_info in cart:
+                name=goods_info[0]
+                num=goods_info[1]
+                price=goods_info[2]
+                total += price * num
+                stock = int(cf.get("库存", name))
+                stock -= num
+                stock_dict[name]=str(stock)
+            if balance >= total:
+                balance -= total
+                buy_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                for goods_info in cart:
+                    name_buy=goods_info[0]
+                    num = goods_info[1]
+                    goods_buy = (name_buy, num, buy_time)
+                    order.append(goods_buy)
+                cart.clear()
+                for k in stock_dict:
+                    cf.set("库存", k, stock_dict[k])
+                cf.set(user, "order", str(order))
+                cf.set(user, "salary", str(balance))
+                cf.set(user, "cart", str(cart))
+                cf.write(open(db, "w"))
+                print("购物车商品%s已全部结算，详细请查看您的订单！" % str(name))
+            else:
+                print("抱歉，您的余额【%d元】不足以支付订单【%d元】" % (balance,total))
+        elif opera.upper() == "B":
+            hand=input("请问您是要修改商品数量还是要删除商品呢? A 修改商品数量 B 删除商品")
+            if hand.upper() == "A":
+                number=int(input("\033[37;1m请输入您要修改的商品的编号:\033[0m"))
+                print("现在购物车里有[%d]件[%s]" % (cart[number][1],cart[number][0]))
+                num_new=input("\033[37;1m请输入您要购买的数量:\033[0m")
+                cart[number][1]=int(num_new)
+            if hand.upper() == "B":
+                number=int(input("\033[37;1m请输入您要删除的商品的编号:\033[0m"))
+                cart.pop(number)
+            cf.set(user, "cart", str(cart))
+            cf.write(open(db, "w"))
+            print("修改购物车成功!")
+        elif opera.upper() == "C":
+            cart.clear()
+            cf.set(user, "cart", str(cart))
+            cf.write(open(db, "w"))
+            print("清空购物车成功!")
+        elif opera.upper() == "D":
+            exit_flag=True
+        elif opera.upper() == "E":
+            cart = eval(cf.get(user, "cart"))
+            order = eval(cf.get(user, "order"))
+            balance = int(cf.get(user, "salary"))
+            print("\033[33;7m您的购物车如下\033[0m".center(50))
+            print("编号", "商品".center(20), "数量".center(20), "价格")
+            for index, items in enumerate(cart):
+                print(index, items[0].center(20), str(items[1]).center(20), items[2])
+        else:
+            print("您的输入有误,请重新输入!")
+#充值函数,可以查看余额,也可以充值
+def charge_func(user):
+    balance=int(cf.get(user,"salary"))
+    print("您当前的余额为[%d元]" % balance)
+    while True:
+        money=input("\033[37;1m请输入您要充值的金额:\033[0m")
+        if money.isdigit():
+            balance+=int(money)
+            cf.set(user,"salary",str(balance))
+            cf.write(open(db,"w"))
+            print("恭喜您充值成功!余额为[%d元]" % balance)
+            break
+        else:
+            print("您输入的有误,请重新输入!")
+
+#shop_func("zy")
+#cart_func("zy")
+#record_func("zy")
+#charge_func("zy")
+
+if __name__ == "__main__":
+    mail_check()
+    login_info=login()
+    login_flag=login_info[0]
+    username=login_info[1]
+    while True:
+        operate=input("\033[37;1m请输入您要进行的操作:A 购物 B 进入购物车 C 查看购物信息 D 充值 E 退出 \033[0m")
+        if operate.upper() == "A":
+            shop_func(username)
+        if operate.upper() == "B":
+            cart_func(username)
+        if operate.upper() == "C":
+            record_func(username)
+        if operate.upper() == "D":
+            charge_func(username)
+        if operate.upper() == "E":
+            sys.exit("欢迎您的下次光临")
+
+
+
